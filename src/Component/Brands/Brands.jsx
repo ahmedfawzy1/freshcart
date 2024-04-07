baf94715a4ad7598c7e57f344c467c27033a2265
Ahmed Fawzy <ahmedfawzy100038@gmail.com>
2024-04-07 12:15:34 +0200

    update status in categories and brands

--- src/Component/Brands/Brands.jsx
+++ src/Component/Brands/Brands.jsx
@@ -2,34 +2,49 @@ import React, { useEffect, useState } from "react";
 import style from "./Brands.module.css";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
+import { useQuery } from "react-query";
 
 export default function Brands() {
-  const [brands, setBrands] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [brands, setBrands] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
   async function getbrands() {
-    let { data } = await axios.get(`https://ecommerce.routemisr.com/api/v1/brands`);
-    setBrands(data?.data);
-    setLoading(false);
+    return await axios.get(`https://ecommerce.routemisr.com/api/v1/brands`);
   }
+  let { data, refetch, isLoading } = useQuery(["Brands"], () => getbrands(), {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
+  // setBrands(data?.data);
+  // setLoading(false);
 
   useEffect(() => {
-    getbrands();
-  }, []);
+    refetch();
+  }, [refetch]);
   return (
     <>
       <section className="py-5">
         <div className="container">
           <h3 className="fw-bold text-center">Brands</h3>
-          {loading ? (
+          {isLoading ? (
             <div className="container">
               <div className="row align-items-center vh-100">
-                <ThreeDots visible={true} height={100} width={100} color="#0aad0a" radius="9" ariaLabel="three-dots-loading" wrapperStyle={{}} wrapperClass="justify-content-center" />
+                <ThreeDots
+                  visible={true}
+                  height={100}
+                  width={100}
+                  color="#0aad0a"
+                  radius="9"
+                  ariaLabel="three-dots-loading"
+                  wrapperStyle={{}}
+                  wrapperClass="justify-content-center"
+                />
               </div>
             </div>
           ) : (
             <div className="row gy-3">
-              {brands.map((brands) => (
+              {data?.data.data.map((brands) => (
                 <div key={brands?._id} className={`col-md-2 ${style.screenSm}`}>
                   <div className="item text-center">
                     <img src={brands?.image} className="w-100" alt={brands?.name} />
